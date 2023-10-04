a187a98d32afa800873400169f685d7eade78922
radinkaaa <radinkasayhoutra21@students,amikom.ac.id>
2023-10-04 15:54:33 +0700

    new

---
+++ app/src/main/java/com/example/projectbp2674/BookKidActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectbp2674
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookKidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_kid)
+    }
+}
